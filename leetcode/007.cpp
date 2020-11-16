0f6f4e95ca660817157dfadfe03f53774f71c15e
bovenson <szhkai@qq.com>
2020-11-16 22:28:18 +0800

    add leetcode 007

--- leetcode/007.cpp
+++ leetcode/007.cpp
@@ -5,12 +5,21 @@
 using namespace std;
 
 class Solution {
+    int INT_MAX_DEC = INT_MAX / 10;
+    int INT_MIN_DEC = INT_MIN / 10;
+    int INT_MAX_LAST = INT_MAX % 10;
+    int INT_MIN_LAST = INT_MIN % 10;
 public:
     int reverse(int x) {
         int res = 0;
         while (x != 0) {
             int pop = x % 10;
-            if (res res)
+            if (res > INT_MAX_DEC || (INT_MAX_DEC == res && INT_MAX_LAST < pop))
+                return 0;
+            if (res < INT_MIN_DEC || (INT_MIN_DEC == res && INT_MIN_LAST < pop))
+                return 0;
+            res = res * 10 + pop;
+            x /= 10;
         }
         return res;
     }
