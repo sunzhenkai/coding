a7a7b46837d684f59383929f10a9909d615a6319
wii <zhenkai.sun@qq.com>
2021-01-16 01:00:06 +0800

    add 1004

--- hdoj/1004.cpp
+++ hdoj/1004.cpp
@@ -7,7 +7,7 @@
  *
  * @email  zhenkai.sun@qq.com
  *
- * @date   Thu Jan 14 10:40:10 CST 2021
+ * @date   Thu Jan 14 23:52:59 CST 2021
  */
 
 #include <iostream>
@@ -19,5 +19,22 @@
 using namespace std;
 
 int main() {
+    int n;
+    while (cin >> n && n > 0) {
+        map<string, int> m;
+        string ts, res;
+        int mx = 0;
+        for (int i = 0; i < n; ++i) {
+            cin >> ts;
+            if (m.find(ts) != m.end()) m[ts] = m[ts] + 1;
+            else m[ts] = 1;
+
+            if (mx < m[ts]) {
+                mx = m[ts];
+                res = ts;
+            }
+        }
+        cout << res << endl;
+    }
     return 0;
 }
