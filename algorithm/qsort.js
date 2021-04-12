0351dcfd6d2044940a6c6fe2ffee74c3c3e1736b
wii <zhenkai.sun@qq.com>
2021-04-12 19:55:34 +0800

    add comments

--- algorithm/qsort.js
+++ algorithm/qsort.js
@@ -1,3 +1,9 @@
+/**
+ * 快速排序
+ * @param nums  数组
+ * @param left  排序的左边界索引
+ * @param right 排序的右边界索引
+ */
 function qsort(nums, left, right) {
     if (left >= right) return;
 
@@ -18,3 +24,12 @@ var v;
 v = [3, 2, 1, 3, 4, 0, -1, 8, 9, 1, 0];
 qsort(v, 0, v.length - 1);
 console.log(v);
+
+v = [];
+qsort(v, 0, v.length - 1);
+console.log(v);
+
+v = [5, 4, 3, 2, 1];
+qsort(v, 0, v.length - 1);
+console.log(v);
+
